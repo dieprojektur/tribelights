cd43e2367d4d86720dde6dbb7d42d8d08d9e3122
Michael Laussegger <michael.laussegger@dieprojektur.com>
2010-07-20 12:19:04 +0200

    public profile saving (not working)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,10 +5,24 @@ class Profile
   embeds_one :name
   accepts_nested_attributes_for :name
   referenced_in :user
+  references_one :public_profile
+  #after_save :create_public_profile
   validates_presence_of :user
   before_save :check_and_initialize_name
+
   protected
   def check_and_initialize_name
     self.create_name unless self.name
+  end
+
+  def create_public_profile
+    if self.public_profile.nil? then
+      self.public_profile = PublicProfile.create(:name => self.name.full,
+       :birthday => self.birthday, :profile => self)
+    else
+      self.public_profile.attributes =  {:name => self.name.full,
+       :birthday => self.birthday}
+      self.public_profile.save
+    end
   end
 end
